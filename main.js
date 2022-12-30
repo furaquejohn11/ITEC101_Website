837304cb6ebe7987bc8a6aade1d898e921672a64
John Patrick Furaque <102823750+furaquejohn11@users.noreply.github.com>
2022-12-31 03:19:35 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+btnSideBar =() => {
+    let menu = document.getElementById("main-con-sidebar");
+    if (menu.style.display == "none") {
+        menu.style.display = "block";
+    }
+    else {
+        menu.style.display = "none"
+    }
+    
+
+}
